c5585f3d75b0945cfb829e621e69ae7db9137cc1
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-06 20:07:46 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter radius : ");
+int radius;
+radius = Convert.ToInt32(Console.ReadLine());
+double area, length;
+area = Math.PI*radius*radius;
+length = 2 * Math.PI * radius;
+Console.WriteLine("S = "+area+" , "+"L = "+length);
+
+Console.ReadKey();
