4ccddd14efc106b67d7168d232bdfebc35bd5d21
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-12 14:42:54 +0500

    COMPLETE

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter radius : ");
+/*Console.Write("Enter radius : ");
 int radius;
 radius = Convert.ToInt32(Console.ReadLine());
 double area, length;
@@ -7,4 +7,18 @@ area = Math.PI*radius*radius;
 length = 2 * Math.PI * radius;
 Console.WriteLine("S = "+area+" , "+"L = "+length);
 
-Console.ReadKey();
+Console.ReadKey();*/
+
+// 2. Valyuta konvertatsiya
+/*Console.Write("Summani kiriting : ");
+int summa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Kursni kiriting : ");
+int kurs = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Summa : "+summa*kurs+" so'm");*/
+
+// 3.Yoshni hisoblash
+Console.Write("Tugilgan yilingiz kiriting : ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+DateTime nowDate = DateTime.Now;
+Console.WriteLine((nowDate.Year - userAge)*365);
